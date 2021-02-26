d8f9c05252c04c0a7bcfec61f0ff9229a3fc1a0b
Guilherme Gomes <guingomes@gmail.com>
2021-02-26 14:51:03 -0300

    estudos e treinos

---
+++ 4.funcoes.cpp
@@ -0,0 +1,46 @@
+//objetivo é aprender a programar as primeiras funções em linguagem C++.
+
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.14; //define serve para definir uma variavel imutável. Interessante para repetições
+#define curtir cout << "Curta essa pagina!"
+
+//void significa que não será retornado nada. para retornar, repetir a função dentro do main
+void inscrever(){
+    cout << "Aprendizado da linguagem de programacao em C++" << endl;;
+}
+
+int somar(int x, int y){
+    int t;
+    t=1;
+
+    x=30; //definindo o x sendo 30 não irá mudar o valor de a. Entretanto, irá interferir na soma.
+
+    int soma;
+    soma = x+y;
+    return soma;
+}
+
+int main(){
+    int t;
+    t = 0;
+
+    inscrever();
+
+    int a=5, b=4;
+    int s;
+    s = somar(a, b);
+    cout << "Valor de a: " << a << endl;
+
+    cout << "Soma: " << s << endl;
+
+    cout << "Valor de t: " << t << endl;
+
+    //cout << pi;
+
+    curtir;
+
+    return 0;
+}
